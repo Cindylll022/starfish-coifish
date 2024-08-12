24b6b5baa31b26502e6debc563591d59bb3a502e
cindylll022 <cindyllllll022@gmail.com>
2024-08-12 16:09:31 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,20 +1,21 @@
-// background.js
-const terms = ["Terms of Service", "TOS", "Privacy Policy", "Terms and Conditions", "User Agreements", "terms of service", "privacy policy", "terms and conditions", "user agreements"];
+const terms = [
+  "Terms of Service", "TOS", "Privacy Policy", "Terms and Conditions", 
+  "User Agreements"];
 
-// Function to check if any of the terms appear in the text content of the document
+// Function to check if any of the terms appear in the text content
 function containsTerms(text, terms) {
-  return terms.some(term => text.includes(term));
+  return terms.some(term => text.toLowerCase().includes(term.toLowerCase()));
 }
+
+// Listener for messages from content scripts
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.textContent) {
     const textContent = message.textContent;
-    console.log("message sent")
-    
+    console.log("Message received");
 
     if (containsTerms(textContent, terms)) {
       chrome.storage.local.set({ termsDetected: true }, () => {
         console.log('Terms detected and stored');
-        // Set a badge to alert the user
         chrome.action.setBadgeText({ text: '!' });
         chrome.action.setBadgeBackgroundColor({ color: '#FFD900' });
       });
@@ -27,13 +28,14 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-    chrome.tabs.get(activeInfo.tabId, function(tab) {
-        // Inject your content script into the active tab
-        chrome.scripting.executeScript({
-            target: {tabId: activeInfo.tabId},
-            files: ['scripts/content.js']
-        });
-    });
-  console.log("tab switched");
+// Listener for tab activation
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.scripting.executeScript({
+    target: {tabId: activeInfo.tabId},
+    files: ['scripts/content.js']
+  }).then(() => {
+    console.log("Content script injected");
+  }).catch((error) => {
+    console.error("Error injecting content script: ", error);
+  });
 });
