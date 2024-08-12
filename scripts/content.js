72c832812c1955d0e4b6296f463e158be8d048b1
cindylll022 <cindyllllll022@gmail.com>
2024-08-12 16:38:58 -0700

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,10 @@
-// Get the text content of the entire document
-const textContent = document.body.textContent || document.body.innerText;
-console.log(textContent)
-// Send the text content to the background script
-chrome.runtime.sendMessage({ textContent: textContent });
+(function() {
+  // Encapsulated code
+
+  // Get the text content of the entire document
+  const textContent = document.body.textContent || document.body.innerText;
+  console.log(textContent);
+
+  // Send the text content to the background script
+  chrome.runtime.sendMessage({ textContent: textContent });
+})();
