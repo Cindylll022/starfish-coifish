32a19d1b6bb179e6f7bb6a4c21767aaee3f7a997
stephaniecii <142693906+stephaniecii@users.noreply.github.com>
2024-09-07 14:09:17 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // retrieves and checks the termsDetected from local chrome storage
   chrome.storage.local.get(['termsDetected'], (result) => {
     const messageElement = document.getElementById('message');
+    //updates message element with result.termDetected boolean result
     if (result.termsDetected) {
-      messageElement.textContent = 'Terms detected!';
+      messageElement.textContent = "Terms detected";
     } else {
-      messageElement.textContent = 'No terms detected.';
+      messageElement.textContent = "No terms detected.";
     }
   });
 });
